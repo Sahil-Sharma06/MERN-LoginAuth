651e78451565fcb95dc736568cf4c79b8bc5253d
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-03-21 07:54:46 +0530

    Refactor components and add .gitignore file

--- Login/src/Components/Dashboard.jsx
+++ Login/src/Components/Dashboard.jsx
@@ -1,35 +1,3 @@
-// import React, { useEffect } from 'react';
-// import axios from 'axios';
-// import { useNavigate } from 'react-router-dom'; // Import useNavigate
-
-// const Dashboard = () => {
-//     const navigate = useNavigate(); // Use useNavigate hook to get the navigation function
-//     axios.defaults.withCredentials = true
-//     useEffect(() => {
-//         axios.get('http://localhost:3000/auth/verify')
-//             .then(res => {
-//                 if (res.data.status) {
-                    
-//                 } else {
-//                     navigate('/'); // Navigate to home if user is not authorized
-//                 }
-//             })
-//             .catch(error => {
-//                 console.error('Error verifying user:', error);
-//                 navigate('/'); // Navigate to home in case of error
-//             });
-//     }, []);
-
-//     return (
-//         <div>
-//             Dashboard
-//         </div>
-//     );
-// };
-
-// export default Dashboard;
-
-
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
--- Login/src/Components/Home.jsx
+++ Login/src/Components/Home.jsx
@@ -1,35 +1,3 @@
-// import React, {useState} from 'react'
-// import '../App.css'
-// import {Link} from 'react-router-dom'
-
-// const Home = () =>{
-//   const navigate = useNavigate()
-//   Axios.default.withCrendentials = true;
-//   const handleLogout = () =>{
-//     Axios.get('https://localhost:3000/auth/logout')
-//     .then(res=>{
-//       if(res.data.status){
-//         navigate('/login')
-//       }
-//     }).catch(err=>{
-//       console.log(err)
-//     })
-//   }
-
-
-//   return (
-//     <div>
-      
-//       <button><Link to="/dashboard">Dashboard</Link></button>
-//       <br></br>
-//       <button onClick={handleLogout}>Logout</button>
-      
-//     </div>
-//   )
-// }
-
-// export default Home
-
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
 import Axios from 'axios'; // Import Axios
